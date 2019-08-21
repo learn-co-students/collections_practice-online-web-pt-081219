cc4710d65b01e630046fede00ec9198e69b15f10
Christian Cain <christian22cain@yahoo.com>
2019-08-21 04:07:30 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,78 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    elsif a < b
+      1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length > b.length
+      1
+    elsif a.length < b.length
+      -1
+    end
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    if a == array[1]
+      1
+    else
+      0
+    end
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  
+  array.each do |element|
+    element[2] = "$"
+    new_array.push(element)
+  end
+  
+  new_array
+end
+
+def find_a(array)
+  results_array = []
+  
+  array.each do |element|
+    if element[0] == "a"
+      results_array.push(element)
+    end
+  end
+  
+  results_array
+end
+
+def sum_array(array)
+  array.inject{|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index != 1
+      element.concat("s")
+    else
+      element
+    end
+  end
+end
