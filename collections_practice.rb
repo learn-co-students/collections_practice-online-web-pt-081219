8d845ca7e489a1258ced578d194dd02bfa55aed3
Jeffrey Brown <brownjs03@gmail.com>
2019-08-28 02:53:05 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+def sort_array_asc(array)
+  array.sort 
+end 
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.size <=> b.size 
+end
+end
+
+def swap_elements(array)
+  new = []
+  new = array
+  array[1], array[2] = array[2], array[1]
+  new
+end 
+
+def reverse_array(array)
+  new = []
+  new = array.reverse 
+  new 
+end 
+
+def kesha_maker
+end 
+
+
+def find_a(array)
+  array.select {|i| i.start_with?("a")}
+end
+
+def sum_array(array)
+ array.inject(0) {|num, x| num + x}
+end 
+
+def add_s(array
+  new << array.each.collect {|word| word + "s"}
+end
