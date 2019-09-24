bc31f8e675983d432789c30a8cc2bd150273dc01
Darin Webster <darinleenavy@gmail.com>
2019-09-24 09:54:26 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,11 +15,6 @@ def swap_elements(strings)
   return strings
 end
 
-def swap_elements_from_to(array, index, destination_index)
-  array[index], array[destination_index] = array[destination_index], array[index]
-  return array
-end
-
 def reverse_array(integers)
   new_array = integers.reverse
   new_array
